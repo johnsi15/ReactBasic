17ae1e81559d3e835435cb69fd0e0138d8ebf9f5
John serrano <andrey_201015@hotmail.com>
2017-08-09 18:02:40 -0500

    Agregando example 3

--- redux.js
+++ redux.js
@@ -1,3 +1,4 @@
+// Este en teoria es el reducer donde estas las opciones dependiendo del type de action.
 const counter = (state = 0, action) => {
   switch (action.type){
     case 'INCREMENT':
@@ -10,7 +11,7 @@ const counter = (state = 0, action) => {
 }
 
 const { createStore } = Redux;
-// var createStore = Redux.createStore;
+// var createStore = Redux.createStore(counter);
 // import { createStore } from 'redux';
 const store = createStore(counter);
 
@@ -52,19 +53,40 @@ const Counter = ({
   </div>
 );
 
+const increment = {
+  type: 'INCREMENT'
+}
+
+const decrement = {
+  type: 'DECREMENT'
+}
+
 const render = () => {
+  // ReactDOM.render(
+  //   <Counter
+  //     value={ store.getState() }
+  //     onIncrement={ () =>
+  //       store.dispatch({
+  //         type: 'INCREMENT'
+  //       })
+  //     }
+  //     onDecrement={ () =>
+  //       store.dispatch({
+  //         type: 'DECREMENT'
+  //       })
+  //     }
+  //   />,
+  //   document.getElementById('app')
+  // );
+
   ReactDOM.render(
     <Counter
       value={ store.getState() }
       onIncrement={ () =>
-        store.dispatch({
-          type: 'INCREMENT'
-        })
+        store.dispatch(increment)
       }
       onDecrement={ () =>
-        store.dispatch({
-          type: 'DECREMENT'
-        })
+        store.dispatch(decrement)
       }
     />,
     document.getElementById('app')
@@ -73,3 +95,41 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
+// Nota: El estado deberia siempre mantenerse es decir nunca mutarlo sino que el estado anterior permanesca,
+// Y solo se agregue un nuevo estado a un array.
+
+/*
+  Example 3
+*/
+
+// Mantemos siempre el state anterior en este caso.
+const reducer = (state = [], action) => {
+  switch (action.type){
+    case 'add_to_array':
+      return [action.payload];
+    case 'add_one_more':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
+
+const { createStore } = Redux;
+// var createStore = Redux.createStore(counter);
+// import { createStore } from 'redux';
+const store = createStore(reducer);
+
+store.getState();
+
+const action = {
+  type: 'add_to_array',
+  payload: 'ReduxIsAwesome'
+}
+const action2 = {
+  type: 'add_one_more',
+  payload: 'And fun'
+}
+store.dispatch(action);
+store.dispatch(action2);
+document.body.innerHTML = store.getState();
